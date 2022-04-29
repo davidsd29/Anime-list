ec3c975835ad4e203f4dc9d67c63902c3d156581
davidsd29 <davidsdankwah@gmail.com>
2022-04-29 02:52:36 +0200

    website styling

--- project-tech/scss.sh
+++ project-tech/scss.sh
@@ -1,1 +1,7 @@
 sass --watch assets/scss/style.scss assets/css/style.css
+
+    #    <!-- <ul>
+    #     <% anime.catagorie.forEach(function(catagorie){ %>
+    #     <%- include('loop/catagories', {catagorie: catagorie}); %>
+    #     <% }); %>
+    # </ul> -->
