bda88b3edfb7da645c65ea14e3cec4da96c12d42
davidsd29 <davidsdankwah@gmail.com>
2022-05-23 11:51:49 +0200

    Repository op orde

--- project-tech/assets/js/API/quotes.js
+++
@@ -1,9 +0,0 @@
-const quoteInput = document.getElementById("quote-text"),
-     name = document.getElementById("quote-name")
-
-fetch('https://animechan.vercel.app/api/random')
-    .then(response => response.json())
-    .then(quote => {
-        quoteInput.textContent = `"${quote.quote}"`;
-        name.textContent = `~${quote.character}~`;
-    })
